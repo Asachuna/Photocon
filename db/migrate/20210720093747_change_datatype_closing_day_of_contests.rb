19a2c0677f2a0a4a4151b0932a366c2f79970279
Asachun <yugoka5@gmail.com>
2021-07-26 06:46:36 +0000

    for heroku

--- db/migrate/20210720093747_change_datatype_closing_day_of_contests.rb
+++ db/migrate/20210720093747_change_datatype_closing_day_of_contests.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeClosingDayOfContests < ActiveRecord::Migration[6.1]
   def change
-    change_column :contests, :closing_day, 'integer USING CAST(closing_day AS integer)'
+    change_column :contests, :closing_day, :integer
   end
 end
